2ff9cbacafb3600eb8172c9c4966ca16e4b5114c
Peter Petersson <petersson.peter@gmail.com>
2012-02-29 20:51:33 +0100

    Fixing typo in comment

--- src/main/scala/net/liftmodules/FoBo/snippet/FoBo/ScriptHelper.scala
+++ src/main/scala/net/liftmodules/FoBo/snippet/FoBo/ScriptHelper.scala
@@ -52,15 +52,10 @@ class ScriptHelper extends StatefulSnippet with Loggable {
    *                      oldonload();
    *                   }
    *                   func();
-<<<<<<< HEAD
+
    *                }
    *             }
    *          }
-=======
-   *             }
-   *          }
-   *       ;
->>>>>>> branch 'master' of git@github.com:karma4u101/FoBo.git
    *    // ]]>  
    *  </script>
    * }}}
