5356abf0751729d1a213a9ca7acca5b5c6ad1a68
karma4u101 <petersson.peter@gmail.com>
2014-06-01 15:40:06 +0200

    bumping sbt to 0.13.5

--- project/autobuildscript/experimental.sh
+++ project/autobuildscript/experimental.sh
@@ -3,7 +3,7 @@
 #####
 #
 # This script will, given a Lift target, ensure that the right jdk is set (in a ubuntu system) 
-# and the run the module build as specified in respective Lift targets command files.
+# and then run the module build as specified in respective Lift targets command files.
 # The command files is set up to clean, compile, test and if compile and tests are successful, 
 # that is, correct jvm and java is set up and there are no error in compile or failing tests the    
 # build will proceed with signing and publishing the artifacts.
@@ -19,17 +19,27 @@ while getopts t: opts; do
 done
 
 if [ $LIFT_TARGET == "2.5" ]; then {
-	echo "Target is set to 2.5";
+	echo "Target is set to Lift v2.5";
+	echo -e "Update java: setting java-7-openjdk-amd64 "
 	sudo update-alternatives --set java /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java
-	sbt < ./project/autobuildscript/build-publish-release.txt > build.log	
-} elif [ $LIFT_TARGET == "2.6" ]; then 
-	echo "Target is set to 2.6";
+	echo -e "\n***Start processing the sbt command file.***\n" 
+	sbt < ./project/autobuildscript/build-publish-release.txt > build.log
+	echo -e "\n***...done processing the sbt command file***\n"  	
+} elif [ $LIFT_TARGET == "2.6" ]; then {
+	echo "Target is set to Lift v2.6";
+	echo -e "Update java: setting java-6-openjdk-amd64 "
 	sudo update-alternatives --set java /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java
+	echo -e "\n***Start processing the sbt command file.***\n" 
 	sbt < ./project/autobuildscript/build-publish-SNAPSHOT.txt > build.log
-elif [ $LIFT_TARGET == "3.0" ]; then 
-	echo "Target is set to 3.0";
+	echo -e "\n***...done processing the sbt command file***\n"  
+} elif [ $LIFT_TARGET == "3.0" ]; then { 
+	echo -e "\nTarget is set to Lift v3.0\n";
+	echo -e "Update java: setting java-7-openjdk-amd64 "
 	sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java 
-	sbt < ./project/autobuildscript/build-publish-Lift3-SNAPSHOT.txt > build.log 
-else 
+	echo -e "\n***Start processing the sbt command file.***\n"  
+	sbt < ./project/autobuildscript/build-publish-Lift3-SNAPSHOT.txt >> build.log 
+	echo -e "\n$?"
+	echo -e "\n***...done processing the sbt command file***\n"  
+} else 
 	echo "No target set! You need to specify a Lift version as target. Usage xxxxx -t [2.5,2.6,3.0]";
 fi
