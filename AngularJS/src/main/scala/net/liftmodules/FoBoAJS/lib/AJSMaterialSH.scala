c43ebf9641280268b10cc5f548d62593e274cd42
karma4u101 <petersson.peter@gmail.com>
2015-11-24 10:01:20 +0100

    WIP Integrating the angular material design tosts component into Lift messages.

--- AngularJS/src/main/scala/net/liftmodules/FoBoAJS/lib/AJSMaterialSH.scala
+++ AngularJS/src/main/scala/net/liftmodules/FoBoAJS/lib/AJSMaterialSH.scala
@@ -1,10 +1,14 @@
 package net.liftmodules.FoBoAJS.lib
 
+import net.liftweb.util._
+
 /**
  * @author peter
  */
 class AJSMaterialSH extends ScriptHelper {
  
+  //due to the use of default arguments we can't use overloaded functions (same name) as the compiler can't handle it
+  
   def mdToastWrapMsg(msg:String, position:String="bottom left" ,theme:String=""):scala.xml.NodeSeq = {
     val initFunc = """showSimpleToast('%s','%s','%s');""".format(msg,position,theme)
     return <div ng-controller='LiftMsgToastCtrl' ng-init={initFunc}></div>
@@ -14,5 +18,11 @@ class AJSMaterialSH extends ScriptHelper {
     val initFunc = """showSimpleToast('%s','%s','%s');""".format(msg,position,theme)
     return <div ng-controller='LiftMsgToastCtrl' ng-init={initFunc}></div>
   }
+  
+  def mdToastWrapFEMsg(vi: List[FieldError], position:String="bottom left" ,theme:String=""):scala.xml.NodeSeq = {
+    val msg:scala.xml.NodeSeq = (vi.map{ i => i.msg}).lift(0).getOrElse(<span>Something whent wrong!</span>);
+    val initFunc = """showSimpleToast('%s','%s','%s');""".format(msg,position,theme)
+    return <div ng-controller='LiftMsgToastCtrl' ng-init={initFunc}></div>
+  }  
   
 }
