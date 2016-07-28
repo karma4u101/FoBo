681b9ae4f6722aaa927748b137f5c3e4a7073a0d
karma4u101 <petersson.peter@gmail.com>
2016-07-28 19:07:05 +0200

    Adding jquery migrate v1.4.1 to fobo/fobo with tests.

--- JQuery/JQuery/src/main/scala/net/liftmodules/FoBoJQ/FoBoJQ.scala
+++ JQuery/JQuery/src/main/scala/net/liftmodules/FoBoJQ/FoBoJQ.scala
@@ -79,6 +79,23 @@ package object FoBoJQ {
     override def toString() = "FoBoJQ.Toolkit = "+store.toString()
     
    /**
+     * Enable usage of FoBo's JQuery-Migrate API and resources version 1&#8228;4&#8228;1 in your bootstrap liftweb Boot.
+     * @version 1.4.1
+     * 
+     * '''Example:'''
+     * 
+     * {{{
+     *   import net.liftmodules.{FoBoJQ => FoBo}
+     *    :
+     *   FoBo.Toolkit.Init=FoBo.Toolkit.JQueryMigrate141
+     * }}}
+     */    
+     case object JQueryMigrate141 extends Toolkit {
+       FoBoJQRes.Resource.JQueryMigrate141
+       //FoBoJQAPI.API.JQuery1
+     }
+    
+   /**
      * Enable usage of FoBo's JQuery API and resources version 2&#8228;2&#8228;4 in your bootstrap liftweb Boot.
      * @version 2.2.4
      * OBS! JQuery 2.x dose not support Internet Explorer 6, 7, or 8.
@@ -251,6 +268,22 @@ package object FoBoJQ {
     } 
     override def toString() = "FoBoJQ.Resource = "+store.toString()
 
+   /**
+     * Enable usage of FoBo's JQuery-Migrate resources version 1&#8228;4&#8228;1 in your bootstrap liftweb Boot.
+     * @version 1.4.1
+     * 
+     * '''Example:'''
+     * 
+     * {{{
+     *   import net.liftmodules.{FoBoJQ => FoBo}
+     *    :
+     *   FoBo.Resource.Init=FoBo.Resource.JQueryMigrate141
+     * }}}
+     */    
+     case object JQueryMigrate141 extends Resource {
+       FoBoJQRes.Resource.JQueryMigrate141
+     } 
+    
    /**
      * Enable usage of FoBo's JQuery resources version 2&#8228;2&#8228;4 in your bootstrap liftweb Boot.
      * @version 2.2.4
