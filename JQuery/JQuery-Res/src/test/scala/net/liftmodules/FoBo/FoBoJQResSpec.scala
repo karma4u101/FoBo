82fb8c3a10790c2e2738865aa2c9a23f56477770
karma4u101 <petersson.peter@gmail.com>
2016-07-28 16:40:29 +0200

    Adding jquery migrate v1.4.1 and fixing missing dependency on lift-jquery-module.

--- JQuery/JQuery-Res/src/test/scala/net/liftmodules/FoBo/FoBoJQResSpec.scala
+++ JQuery/JQuery-Res/src/test/scala/net/liftmodules/FoBo/FoBoJQResSpec.scala
@@ -9,6 +9,17 @@ object FoBoJQResSpec extends Specification {
   "FoBoJQResSpec Specification".title
 
   sequential
+
+  //JQueryMigrate141
+  "With FoBoJQRes.Resource.Init set to FoBoJQRes.Resource.JQueryMigrate141 the ResourceServer" should {
+    "allow  fobo/jquery-migrate.js" in {
+      allowResource(FoBoJQRes.Resource.JQueryMigrate141,"fobo"::"jquery-migrate.js"::Nil) must_== true 
+    }     
+    "rewrit fobo/jquery-migrate.js to jquery-migrate/1.4.1/js/jquery-migrate-min.js" in {
+      rewriteResource(FoBoJQRes.Resource.JQueryMigrate141,"fobo"::"jquery-migrate.js"::Nil) must_== 
+        List("jquery-migrate", "1.4.1", "js", "jquery-migrate-min.js")
+    }        
+  }   
   
   //JQueryMigrate121
   "With FoBoJQRes.Resource.Init set to FoBoJQRes.Resource.JQueryMigrate121 the ResourceServer" should {
