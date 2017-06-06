9784f4b3987bde16e9c6d5e1965b3baab85de46b
karma4u101 <petersson.peter@gmail.com>
2017-06-06 21:06:56 +0200

    Adding popper and tooltip modules from Popper.js

---
+++ Popper/Popper-API/src/main/scala/net/liftmodules/FoBoPop/FoBoPopAPI.scala
@@ -0,0 +1,73 @@
+package net.liftmodules
+
+import _root_.net.liftweb._
+import util.{Props}
+import http._
+import common._
+
+/**
+  * ==FoBo Popper API Module==
+  *
+  * This FoBo API module provides FoBo/Lift API components for the Popper Toolkit Module,
+  * but can also be used as-is, see below for setup information.
+  *
+  * If you are using this module via the FoBo/FoBo artifact module see also [[net.liftmodules.FoBo]] for setup information.
+  *
+  */
+package object FoBoPopAPI {
+
+  override def toString() = FoBoPopAPI.API.toString()
+
+  /**
+    * Initiate FoBo's Popper API in you bootstrap liftweb Boot.
+    *
+    *  '''Example:'''
+    * {{{
+    *   import net.liftmodules.{FoBoPopAPI => FoBo}
+    *    :
+    *   FoBo.API.Init=FoBo.API.[API Object]
+    * }}}
+    * '''Note:''' To see available objects click on the round trait icon in the header of this page.
+    */
+  abstract sealed trait API
+
+  object API extends API {
+
+    //we don't actually need to store the objects (for now) so lets just save
+    //the object name, we can easily change this if we need to
+    private type Store = List[String] //List[API]
+    private var store: Store = List()
+    def Init: Store = store
+    def Init_=(t: API): Store = {
+      store = if (store contains t.toString) store else t.toString :: store
+      store
+    }
+    override def toString() = "FoBoPopAPI.API = " + store.toString()
+
+    /**
+      * Enable usage of FoBo's Popper API version 1&#8228;X&#8228;X in your bootstrap liftweb Boot.
+      * @version 1.X.X
+      *
+      * '''Example:'''
+      *
+      * {{{
+      *   import net.liftmodules.{FoBoPopAPI => FoBo}
+      *    :
+      *   FoBo.API.Init=FoBo.API.Popper1
+      * }}}
+      */
+    case object Popper1 extends API {
+      FoBoAPI.init
+    }
+  }
+
+  /**
+    * Object for initiating FoBo API packages.
+    */
+  private object FoBoAPI {
+    lazy val init: Unit = {
+      LiftRules.addToPackages("net.liftmodules.FoBoPop")
+    }
+  }
+
+}
